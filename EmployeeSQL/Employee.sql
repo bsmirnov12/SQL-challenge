ac0901ea54b6205175b799569f6b14b8d894deec
Boris Smirnov <bsmirnov@gmail.com>
2020-04-02 15:16:41 -0400

    Initial upload of data and DB Diagram

---
+++ EmployeeSQL/Employee.sql
@@ -0,0 +1,86 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/5e2XSw
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- SQL Homework
+-- UT-TOR-DATA-PT-01-2020-U-C Assignment
+-- (c) Boris Smirnov
+
+CREATE TABLE "Departments" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INT   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(30)   NOT NULL,
+    "last_name" VARCHAR(30)   NOT NULL,
+    "gender" CHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Dept_managers" (
+    -- every department had several managers (one at a time)
+    -- each manager managed only one department
+    "dept_no" CHAR(4)   NOT NULL,
+    -- every manager is an employee
+    -- employee may be a amnager or may be not
+    "emp_no" int   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "Dept_emp" (
+    -- every department employee is company employee
+    -- there are employees who worked for more then one department
+    "emp_no" INT   NOT NULL,
+    -- every departmnet has many employees
+    -- there are employees who worked in more then one department
+    "dept_no" CHAR(4)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "Titles" (
+    -- an employee may have many titles (one at a time)
+    -- every title have many employees
+    "emp_no" INT   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+CREATE TABLE "Salaries" (
+    -- every salary was paid to one employee
+    -- every employee was paid salary one or more times
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL
+);
+
+ALTER TABLE "Dept_managers" ADD CONSTRAINT "fk_Dept_managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_managers" ADD CONSTRAINT "fk_Dept_managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
