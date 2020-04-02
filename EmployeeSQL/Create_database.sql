6c4d87f1248296fe2c9dd0806ac187edacff95f7
Boris Smirnov <bsmirnov@gmail.com>
2020-04-02 15:44:49 -0400

    Database and table creation scripts

---
+++ EmployeeSQL/Create_database.sql
@@ -0,0 +1,17 @@
+-- Database: Employees
+
+-- DROP DATABASE "Employees";
+
+CREATE DATABASE "Employees"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Canada.1252'
+    LC_CTYPE = 'English_Canada.1252'
+    TABLESPACE = "Homework-9"
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE "Employees"
+    IS 'SQL Homework
+UT-TOR-DATA-PT-01-2020-U-C Assignment #9
+(c) Boris Smirnov';
